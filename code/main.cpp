52fc8da27b80659738c660fb098ce84b235f199b
Anders Pedersen <andehus@ulrik.uio.no>
2017-12-15 00:35:35 +0100

    version 0.6 (without variance)

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,4 @@
-/* Version 0.5 stasonær */
-/*TODO
-  make a matrix c_ij which contains the transaction count (in pairs)
-  should be a symmetric matrix with zero on diagonals*/
+/* Version 0.5 stasonær (this version doesn't have variance and equilibrium tests) */
 #include <stdlib.h>
 #include <iostream>
 #include <random>
@@ -12,10 +9,9 @@ using namespace std;
 using namespace arma;
 void print(const char* file,vector<double> &histogram,int n) {
   ofstream ofile;
-  ofile.open(file);//,ios::out | ios::binary);
+  ofile.open(file);
   for(int i = 0; i<histogram.size(); i++) {
     ofile<<histogram[i]/n<<endl;
-    //ofile<<agent[i]<<" "<<(double)agent[i]/n<<" "<<(int)agent[i]/100<<endl; // del på mc cycles
   }
   ofile.close();
   cout<<"OUTPUT: "<<file<<endl;
@@ -24,39 +20,39 @@ void print(const char* file,vector<double> &histogram,int n) {
 /* outfile N MC Transacions lambda alpha */
 int main(int argc,char** argv) {
   int N,CYCLES,TRANSACTIONS;
-  double lambda,alpha;
+  double lambda,alpha,gamma;
   clock_t start,stop;
   double m0 = 1000;
   double p = 0.0;
   char* outfile;
   /* reading arguments */
-  if(argc == 7) {
+  if(argc == 8) {
     outfile = argv[1];
     N = atoi(argv[2]);
     CYCLES = atoi(argv[3]);
     TRANSACTIONS = atoi(argv[4]);
     lambda = atof(argv[5]);
     alpha = atof(argv[6]);
+    gamma = atof(argv[7]);
     //cout<<argv[0]<<argv[1]<<argv[2]<<argv[3]<<argv[4]<<argv[5]<<argv[6]<<endl;
   }
   else{
-    cout<<"Usage: ./main outfile N MC Transaction lambda alpha"<<endl;
+    cout<<"Usage: ./main outfile N MC Transaction lambda alpha gamma"<<endl;
   }
 
   /* arrays */
-  vector<double> histogram(51,0.0);
-  mat c(N,N,fill::zeros);
-  
+  vector<double> histogram(1001,0.0);
+    
   /* random number generator */
   random_device rd;
   mt19937_64 gen(rd());
   uniform_real_distribution<double>RandomNumberGenerator(0.0,1.0);
 
-  /* progression in console */
+  /* show progression in console */
   int percent = 0;
   int total_percent = 0;
   cout<<"start"<<endl;
-  (CYCLES>=100) ? total_percent=CYCLES/100 : total_percent=CYCLES/1;//should divide by 100
+  (CYCLES>=100) ? total_percent=CYCLES/100 : total_percent=CYCLES/1;
   cout<<total_percent<<endl;
   start = clock();
 
@@ -67,34 +63,33 @@ int main(int argc,char** argv) {
       cout<<" [Processing "<<percent++<<"%]\r";
       cout.flush();
     }
+    double p = 0.0;
+    mat C(N,N,fill::zeros);
+    double cmax = 0.0;
     /*** loop transactions ***/
     for(int e = 0; e<TRANSACTIONS; e++) {
       int i =  (int)(RandomNumberGenerator(gen)*N);
       int j =  (int)(RandomNumberGenerator(gen)*N);
       double epsilon = RandomNumberGenerator(gen);
-      /*if(agent[i] - agent[j] == 0){p = 1.0;}
-      else{p = 2*pow(fabs((agent[i]-agent[j])/double(m0)),-alpha);}*/
-      if(i!=j){
+      if(agent[i] - agent[j] == 0){p = 1.0;}
+      else{p = 2*pow(fabs((agent[i]-agent[j])/double(m0)),-alpha)*(pow((C(i,j)+1)/(cmax+1),gamma));}
+      if(RandomNumberGenerator(gen) <p && i!=j){
 	double temp = agent[i]; //remember to use temp if not mortens way
 	agent[i] = lambda*agent[i]+(1-lambda)*epsilon*(agent[i]+agent[j]); //saving for lambda>0
 	agent[j] = lambda*agent[j]+(1-lambda)*(1-epsilon)*(temp+agent[j]); //saving for lambda>0
+	C(i,j)++;
+	C(j,i)++;
+	double currentInteraction = C(i,j);
+	if(currentInteraction>cmax){cmax = currentInteraction;}
       }
     }//end transactions
     for(int i = 0; i<agent.size(); i++) {
       int index = (int)agent[i]/100;
-      cout<<"Agent: "<<agent[i]<<" "<<index<<endl;
-      if(index<50)
+      if(index<1000)
 	histogram[index]++;//= agent[i];
       else
-	histogram[50]++;//= agent[i]; //overflow
+	histogram[1000]++;//= agent[i]; //overflow
     }
-
-    /* insert into histogram */
-    //for(int i = 0; i<agent.size(); i++) {
-      
-    //}
-    //    agent.clear();
-    
   }//end monte carlo
   print(outfile,histogram,CYCLES);
   stop = clock();
@@ -116,103 +111,4 @@ int main(int argc,char** argv) {
 
 
 
-  
-  /* timing for non-paralized code 
-  clock_t start,stop;
-  start = clock();
 
-  /* number of mc cycles 
-  int n_cycles = 2;//10^4
-  int N = 500; //agents
-  int ntrans = 1;//10000000;//10^7
-  double m0 = 1000; //init money
-  vector<int> transaction_count(N);
-  vector<double> avg_money(N);
-  //vector<double> agent(N);
-  vector<double> expectation_value(N);
-  /* lambda 
-  double lambda = 0.0;//0.0 0.25 0.5 0.9;
-  double alpha = 0.5;
-  /* probability 
-  double p = 0.0;
-  /* matrix init  
-  mat a(N,N,fill::zeros);
-  
-  /* money vector
-  for(int i  = 0; i<=N; i++) {
-    //agent[i] = m0;
-    transaction_count[i] = 0;
-    avg_money[i] = 0.0;
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /*
-  /* random number generator 
-  random_device rd;
-  mt19937_64 gen(rd());
-  uniform_real_distribution<double>RandomNumberGenerator(0.0,1.0);
-  /* progression in console 
-  int percent = 0;
-  int total_percent = n_cycles/1;//should divide by 100
-
-
-  /* loop mc cycles 
-  for(int k = 0; k<n_cycles; k++) {
-    vector<double> agent(N);
-    for(int i = 0; i<N; i++) {
-      agent[i] = m0;
-    }
-    if(k%total_percent == 0) {
-	  cout<<" [Processing "<<percent++<<"%]\r";
-      cout.flush();
-    }    
-    for(int t = 0; t<ntrans; t++){ 
-      int i =  (int)(RandomNumberGenerator(gen)*N);
-      int j =  (int)(RandomNumberGenerator(gen)*N);
-      double epsilon = RandomNumberGenerator(gen);
-      if(agent[i] - agent[j] == 0){
-	p = 1.0;
-      }
-      else {
-	p = 2*pow(fabs((agent[i]-agent[j])/double(m0)),-alpha);
-      }
-      if(i!=j){
-	double temp = agent[i]; //remember to use temp if not mortens way
-	//agent[i] = epsilon*(agent[i]+agent[j]); //no saving
-	//agent[j] = (1-epsilon)*(temp+agent[j]); //no saving
-	agent[i] = lambda*agent[i]+(1-lambda)*epsilon*(agent[i]+agent[j]); //saving
-	agent[j] = lambda*agent[j]+(1-lambda)*(1-epsilon)*(temp+agent[j]); //saving
-      }
-    }//end transactions
-    for(int i = 0; i<N; i++) {
-      expectation_value[i] += agent[i];
-    }
-  }//end mc
-  cout<<endl;
-  /*double sum = 0.0;
-  for(int i = 0; i<agent.size(); i++)
-    sum += agent[i];
-  double avg_m = sum/agent.size();
-  cout<<"Average money: "<<avg_m<<endl;
-  print(agent,n_cycles);
-  *//*
-  print(expectation_value,n_cycles);
-  stop = clock();
-  cout<<"Time: "<<((double)(stop-start)/CLOCKS_PER_SEC)<<endl;
-  return 0;
-}
-*/
